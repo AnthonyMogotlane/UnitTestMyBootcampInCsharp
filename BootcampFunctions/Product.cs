81c657b8a5cc271eec6de917d8f3272e116f9fcb
Anthony <almogotlane@gmail.com>
2022-07-13 15:33:24 +0200

    Added a class of Products to be used to instatiate objects

---
+++ BootcampFunctions/Product.cs
@@ -0,0 +1,7 @@
+namespace BootcampFunctions;
+public class Product
+{
+    public string Department {get; set;} = string.Empty;
+    public int Sales {get; set;}
+    public string Day {get; set;} = string.Empty;
+}

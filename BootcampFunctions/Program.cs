e36254413e7dc8f5f6d11c7052a0f9de8dfcb285
Anthony Mogotlane <almogotlane@gmail.com>
2022-07-13 07:31:45 +0200

    Modified Program.cs

--- BootcampFunctions/Program.cs
+++ BootcampFunctions/Program.cs
@@ -1,3 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using BootcampFunctions;
-Console.WriteLine(YearsAgo.Ago(2020));
+
+Item item1 = new Item(){Name = "Banana", Qty = 20};
+Item item2 = new Item(){Name = "Apple",Qty = 60};
+Item item3 = new Item(){Name = "Grape",Qty = 50};
+
+List<Item> listOfItems = new List<Item>();
+listOfItems.Add(item1);
+listOfItems.Add(item2);
+listOfItems.Add(item3);
+
+foreach (var item in FindItemsOver20.ItemsOver20(listOfItems))
+{
+    Console.WriteLine(item.Name);
+}

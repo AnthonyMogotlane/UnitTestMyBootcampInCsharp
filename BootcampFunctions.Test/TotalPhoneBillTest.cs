46164ff115e9787bc92f49db25a3bd3f9d9a9c33
Anthony <almogotlane@gmail.com>
2022-07-13 16:43:33 +0200

    Modified expected results

--- BootcampFunctions.Test/TotalPhoneBillTest.cs
+++ BootcampFunctions.Test/TotalPhoneBillTest.cs
@@ -5,18 +5,18 @@ public class TotalPhoneBillTest
     [Fact]
     public void ShouldReturnTheTotalBillOfCallsAndSms()
     {
-       Assert.Equal("R7,45", TotalPhoneBill.PhoneBill("call, sms, call, sms, sms"));
+       Assert.Equal("R7.45", TotalPhoneBill.PhoneBill("call, sms, call, sms, sms"));
     }
 
     [Fact]
     public void ShouldReturnTheTotalBillForCalls()
     {
-       Assert.Equal("R11,00", TotalPhoneBill.PhoneBill("call, call, call, call"));
+       Assert.Equal("R11.00", TotalPhoneBill.PhoneBill("call, call, call, call"));
     }
 
     [Fact]
     public void ShouldReturnTheTotalBillForSms()
     {
-       Assert.Equal("R2,60", TotalPhoneBill.PhoneBill("sms, sms, sms, sms"));
+       Assert.Equal("R2.60", TotalPhoneBill.PhoneBill("sms, sms, sms, sms"));
     }
 }
